367377c82ded1a66238c050841e68b6583653a4e
thisisausername87 <165698057+thisisausername87@users.noreply.github.com>
2025-11-07 14:55:08 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+#import plotly.express as px
 
 data_path = os.path.join(os.getcwd(), "data", "degrees_that_pay_back.csv")
 df = pd.readcsv(data_path)
