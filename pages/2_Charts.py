fdadd4b174835a6e22b8b03fe3d246be96e84a06
thisisausername87 <165698057+thisisausername87@users.noreply.github.com>
2025-12-01 16:15:39 -0700

    Update 2_Charts.py

--- pages/2_Charts.py
+++ pages/2_Charts.py
@@ -12,7 +12,7 @@ st.subheader("This page shows various examples of different charts relating to t
 
 fig = px.bar(df_sorted, x="Undergraduate Major", y="Starting Median Salary", title="Starting Salary by Major")
 st.plotly_chart(fig)
-st.write("This bar graph shows the starting median salaries for each undergraduate major. The graph is understood by looking at the value on the left of the graph and correlating it with the major on the bottom under the bar.") 
+st.write("This bar graph shows the starting median salaries for each undergraduate major. The graph is understood by looking at the value on the left of the graph and correlating it with the major on the bottom under the bar. More information for each major can be seen by hovering over the bars.") 
 st.write("This data shows that some of the highest paying majors are physicians assistant, chemical engineering, computer engineering, and electrical engineering. There are no outliers or otherwise unusual data points in the graph. One conclusion that can be found from the information is that STEM related majors that are more mathematically and scientifically technical tend to have higher paying positions as opposed to the more humanitarian jobs such as journalism and music.")
 
 fig = px.histogram(df_sorted, x="Starting Median Salary", nbins=20, title="Counts of Starting Salaries")
