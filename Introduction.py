0e828f9d500f32dea930f1bc8fc38d5cf362b272
thisisausername87 <165698057+thisisausername87@users.noreply.github.com>
2025-12-01 16:30:44 -0700

    Update and rename streamlit_app.py to Introduction.py

---
+++ Introduction.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("Introduction")
+st.write("This website is and analysis of the median salaries for different undergraduate majors. On the left is a set of tabs you can use to navigate and visualize the different relationships of this dataset to better understand not only the pay ranges for different majors, but how they adapt and change over the course of a career.")
--- streamlit_app.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-import pandas as pd
-#import plotly.express as px
-
-data_path = os.path.join(os.getcwd(), "data", "degrees_that_pay_back.csv")
-df = pd.readcsv(data_path)
-df
