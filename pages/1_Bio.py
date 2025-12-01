dc478ab63da6354bf23e6e0d58c6d5a14e82ba46
thisisausername87 <165698057+thisisausername87@users.noreply.github.com>
2025-12-01 05:21:46 -0700

    Update 1_Bio.py

--- pages/1_Bio.py
+++ pages/1_Bio.py
@@ -1,4 +1,12 @@
 import streamlit as st
 
 st.title("My Bio")
-
+st.write("My name is Toren and I am a computer science major at MSU Denver. 
+I take interest in having a greater understanding of the world around me and 
+being able to navigate the complexities that I see around me every day. I 
+utilize python along with various libraries such as Pandas, MatPlotLib, and 
+Plotly to name a few in order to analyze data files into easily understandable 
+and accessible interpretations of their contents. When visualizing data, I 
+believe that it is always best to have the most accurate and well conceptualized 
+data and interpretations possible so that the most truthful possible understandings 
+can be drawn from a strong analysis of information.")
