6a1b44c0f5542a66b3e67020aa2f2b6ea19e1da8
thisisausername87 <165698057+thisisausername87@users.noreply.github.com>
2025-12-06 22:39:32 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 st.title("Introduction")
-st.write("This website is and analysis of the median salaries for different undergraduate majors. On the left is a set of tabs you can use to navigate and visualize the different relationships of this dataset to better understand not only the pay ranges for different majors, but how they adapt and change over the course of a career.")
+st.write("This website is an analysis of video metrics from the youtuber Mr Beast. On the left is a set of tabs you can use to navigate and visualize the different relationships of this dataset to better understand how different components and aspects of a video can impact its performance.")
