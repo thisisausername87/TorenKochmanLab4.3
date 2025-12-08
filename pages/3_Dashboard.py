894ffa662af7833fd4c91757691319b0b3a7247d
thisisausername87 <165698057+thisisausername87@users.noreply.github.com>
2025-12-07 21:27:53 -0700

    Update 3_Dashboard.py

--- pages/3_Dashboard.py
+++ pages/3_Dashboard.py
@@ -3,8 +3,9 @@ import pandas as pd
 import plotly.express as px
 import os
 
+data_path = os.path.join(os.getcwd(), "data", "MrBeast_youtube_stats.csv")
+df = pd.read_csv(data_path)
+
 st.title("Mr Beast Video Analytics")
 st.sidebar.title("Select Visual Charts")
-
-
 st.sidebar.markdown("Select the Charts/Plots accordingly:")
