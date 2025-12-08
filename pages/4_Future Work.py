8cc70abaf12edb0ab175031e9859476260bbbc90
thisisausername87 <165698057+thisisausername87@users.noreply.github.com>
2025-12-07 23:28:18 -0700

    Update 4_Future Work.py

--- pages/4_Future Work.py
+++ pages/4_Future Work.py
@@ -1,3 +1,5 @@
 import streamlit as st
 
-st.write("finish this")
+st.Title("Future Work")
+st.write("For the future of this page, Some of the main features that I would like to add is more interractivity with the sliders and new charts which show potentially different relations between data.")
+st.write("Another addition that could be possible is analyzing more features of the file such as upload date to visualize the increase in poopulatity over time or other interesting trends in the data.")
