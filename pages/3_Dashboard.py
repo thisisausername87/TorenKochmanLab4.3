c9dc399e37023a38a88ee6ba95dafc4d1715403e
thisisausername87 <165698057+thisisausername87@users.noreply.github.com>
2025-12-07 21:27:13 -0700

    Update 3_Dashboard.py

--- pages/3_Dashboard.py
+++ pages/3_Dashboard.py
@@ -4,3 +4,7 @@ import plotly.express as px
 import os
 
 st.title("Mr Beast Video Analytics")
+st.sidebar.title("Select Visual Charts")
+
+
+st.sidebar.markdown("Select the Charts/Plots accordingly:")
